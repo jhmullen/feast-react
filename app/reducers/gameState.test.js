39cd9796ad674c7ec4256a12a007a6a41484967d
Eric Loren <osiris1723@gmail.com>
2017-12-08 06:55:40 -0500

    fixes #4

--- app/reducers/gameState.test.js
+++ app/reducers/gameState.test.js
@@ -1,4 +1,4 @@
-import { buyFood } from '../actions';
+import { addGuest, buyFood } from '../actions';
 import { gameState as reduce, baseState } from './gameState';
 
 const card = {
@@ -27,6 +27,30 @@ describe('BUY_FOOD', () => {
       mana: 0,
       foodDeck: [],
       discard: [card],
+    });
+  });
+});
+
+describe('ADD_GUEST', () => {
+  test('insufficient mana is no-op', () => {
+    const state = {
+      ...baseState,
+      mana: 1,
+      guestDeck: [card],
+    };
+    expect(reduce(state, addGuest(0, 1))).toEqual(state);
+  });
+
+  test('buy deducts mana', () => {
+    const state = {
+      ...baseState,
+      mana: 2,
+      guestDeck: [card],
+    };
+
+    expect(reduce(state, addGuest(0, 1))).toMatchObject({
+      mana: 0,
+      guestDeck: [],
     });
   });
 });
