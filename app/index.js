fabd33c5a6c08066f8b76d1a3933fdfe2a220d9a
Eric Loren <osiris1723@gmail.com>
2017-12-11 06:56:25 -0500

    basic action broadcast

--- app/index.js
+++ app/index.js
@@ -1,23 +1,24 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-import App from "./components/App";
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import feastApp from "./reducers";
-import io from 'socket.io-client'
-import socketLogger from './middleware/socketLogger'
+var React = require('react');
+var ReactDOM = require('react-dom');
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import feastApp from './reducers';
+import io from 'socket.io-client';
+import socketLogger from './middleware/socketLogger';
 
-const socket = io()
-socket.on('connect', function(){console.log('connected')});
+const socket = io();
+socket.on('connect', () => console.log('connected'));
+socket.on('action', action => {
+  console.log('got action', action);
+});
 socket.open();
 
-let store = createStore(feastApp,
-  applyMiddleware(socketLogger(socket))
-);
+let store = createStore(feastApp, applyMiddleware(socketLogger(socket)));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("app")
+  document.getElementById('app'),
 );
--- app/middleware/socketLogger.js
+++ app/middleware/socketLogger.js
@@ -2,4 +2,5 @@ export const socketLogger = socket => store => next => action => {
   socket.emit('action', JSON.stringify(action))
   next(action)
 }
+
 export default socketLogger
